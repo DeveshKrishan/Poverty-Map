cb1e47da63e7ac4749fce89589a4b2869264c7bd
sarvesh-krishan <skrishan@ucdavis.edu>
2023-06-27 19:44:03 -0700

    ran apis and created csv files

--- api.py
+++ api.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
 import numpy as np
 import pandas as pd
 import requests
 
-key = open("key.txt").read()
+def api_request() -> None:
+    """This functions sents multiple GET requests from the years 1995-2020 and generates .csv files for the given years."""
+    key = open("key.txt").read()
 
-print(key)
+    year = np.arange(1995,2021)
+        
+    for _ in year:
+        try:
+            URL = f"https://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEPOVALL_PT,SAEPOVRTALL_PT,SAEMHI_PT,STABREV&for=county:*&time={_}&key={key}"
+            r = requests.get(url = URL).json()
+            print(_)
+            df = pd.DataFrame(r)
+            df.to_csv(f'Dataset/{_}.csv', header=False, index=False)
 
-URL = f"https://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEPOVALL_PT,SAEPOVRTALL_PT,SAEMHI_PT,SAEPOVU_ALL,STABREV&for=county:*&time=2018&key={key}"
+        except:
+            print(f"No data for {_}")
 
+def main():
+    api_request()
 
-
-
-r = requests.get(url = URL)
-print(r.json())
-
+if __name__ == "__main__":
+    main()
