218f2d5a4c05eadef103d0dc6af87208b40f1477
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2023-07-29 20:40:43 -0700

    Grids
    Added Grids

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className="main">
+      <div class="grid-container">
+        <div class="grid-item">Grid 1</div>
+        <div class="grid-item">Grid 2</div>
+        <div class="grid-item long">Grid 3</div>
+        <div class="grid-item long">Grid 4</div>
+        <div class="grid-item">Grid 5</div>
+        <div class="grid-item">Grid 6</div>
+        <div class="grid-item">Grid 7</div>
+        <div class="grid-item">Grid 8</div>
+      </div>
+
+
+    </div>
   )
 }
