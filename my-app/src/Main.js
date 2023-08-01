84acc87f07fd08268cb7d2df58e40438c40d9cb9
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2023-07-31 20:52:51 -0700

    FrontEnd
    Added header and setup for map and map info also began the add parameters

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Main() {
+  const [year, setYear] = useState(1997); // default parameter
+  
+  const handleSliderChange = (event) => {
+    setYear(parseInt(event.target.value));
+  };
+  
   return (
     <div className="main">
       <div class="grid-container">
-        <div class="grid-item">Grid 1</div>
-        <div class="grid-item">Grid 2</div>
-        <div class="grid-item long">Grid 3</div>
-        <div class="grid-item long">Grid 4</div>
-        <div class="grid-item">Grid 5</div>
-        <div class="grid-item">Grid 6</div>
+
+        {/* Header */}
+        <div class="grid-item">
+          <h1 className="title">United States Poverty Rate</h1>
+        </div>
+        <div class="grid-item">
+          <div className="header-2">
+            <a href="#" className="links-huh">Home</a>
+            <a href="#" className="links-huh">About</a>
+          </div>
+        </div>
+
+        {/* MAP */}
+        <div class="grid-item long">
+          <div className="map">
+            MAP
+          </div>
+        </div>
+        {/* MAP INFO */}
+        <div class="grid-item long">
+          <div className="map-info">
+            INFO
+          </div>
+        </div>
+        {/* Map parameters */}
+        <div class="grid-item">
+          <div className="map-parameters">
+            @param
+            <div className="param-1">
+              <input
+              type="range"
+              min="1997"
+              max="2023"
+              value={year}
+              onChange={handleSliderChange}
+              />
+              <p>{year}</p>
+            </div>
+          
+          </div>
+        </div>
+        <div class="grid-item"></div>
+
+        {/* Footer Locations */}
         <div class="grid-item">Grid 7</div>
         <div class="grid-item">Grid 8</div>
       </div>
