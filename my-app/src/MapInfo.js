bfe790dbbc080b49e400e71929c54d393cbe0081
DeveshKrishan <deveshkrishan2003@gmail.com>
2023-08-14 13:19:19 -0700

    test charts

--- my-app/src/MapInfo.js
+++ my-app/src/MapInfo.js
@@ -1,5 +1,6 @@
 import jsonData from "../src/data/county_index.json"
 import stateData from "./data/state_id.json"
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 function MapInfo(props) {
     const county = props.countyName;
@@ -23,6 +24,25 @@ function MapInfo(props) {
         stateName = "";
     }
 
+    let povertyData = [];
+
+    try{
+      const dataInYears = jsonData[uid];
+      
+      for (let i in dataInYears){
+        // console.log(i, dataInYears[i]);
+        const temp = {
+          "year" : i,
+          "poverty" : dataInYears[i]['rate_estimate']
+        };
+      povertyData.push(temp);
+      }
+
+    }
+    catch(e){
+      console.error(`Error has occured : ${e}`);
+    }
+
     return (
       <>
         <p className="map-info-part">County Name: {county}</p>
@@ -31,6 +51,24 @@ function MapInfo(props) {
         <p className="map-info-part">Annual Income: {annualIncome}</p>
         <p className="map-info-part">Poor People: {population}</p>
         {/* Add line graph here  */}
+        {/* margin={{ top: 5, right: 20, bottom: 5, left: 15 }} */}
+        <ResponsiveContainer width={600} height="40%">
+          <LineChart width={500} height={400} fontSize={"1rem"}     margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 15
+          }} data={povertyData} >
+            <Line type="monotone" dataKey="poverty" stroke="#005E84" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+            <Tooltip />
+            <XAxis dataKey="year" interval={1} />
+            <YAxis dataKey="poverty" />
+            <Tooltip />
+          </LineChart>
+      </ResponsiveContainer>
+
+
       </>
     );
   }
